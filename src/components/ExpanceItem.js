c13863a55417f37b0b3f15cbd641bf89721a2817
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-04-06 09:26:34 +0530

    Added location of expance and used variables to assign values to our components

--- src/components/ExpanceItem.js
+++ src/components/ExpanceItem.js
@@ -1,12 +1,17 @@
 // components in react is just a js function//
 import "./ExpanceItem.css";
 function ExpenseItem() {
+  const date = new Date(2021, 3, 22).toString();
+  const expancetitle = "Car Insurance";
+  const price = 444;
+  const locationofexpendature = "any location when we added expance";
   return (
     <div className="expense-item">
-      <div>March 23th 2032</div>
+      <div>{date}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">Rs 444</div>
+        <h3>{locationofexpendature}</h3>
+        <h2>{expancetitle}</h2>
+        <div className="expense-item__price">Rs {price}</div>
       </div>
     </div>
   );
