11cef965252dcdeb3768514f21a8dccdf84c2fbf
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-04-06 12:06:29 +0530

    used map to reduce length of the app.js file

--- src/App.js
+++ src/App.js
@@ -34,30 +34,16 @@ function App() {
   return (
     <div>
       <p>This is also visiable</p>
-      <ExpenseItem
-        title={allexpence[0].title}
-        amount={allexpence[0].amount}
-        date={allexpence[0].date}
-        locationofexpence={allexpence[0].locationofexpence}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={allexpence[1].title}
-        amount={allexpence[1].amount}
-        date={allexpence[1].date}
-        locationofexpence={allexpence[1].locationofexpence}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={allexpence[2].title}
-        amount={allexpence[2].amount}
-        date={allexpence[2].date}
-        locationofexpence={allexpence[2].locationofexpence}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={allexpence[3].title}
-        amount={allexpence[3].amount}
-        date={allexpence[3].date}
-        locationofexpence={allexpence[3].locationofexpence}
-      ></ExpenseItem>
+      {allexpence.map((e) => {
+        return (
+          <ExpenseItem
+            title={e.title}
+            amount={e.amount}
+            date={e.date}
+            locationofexpence={e.locationofexpence}
+          ></ExpenseItem>
+        );
+      })}
       <h2>Let's get started</h2>
     </div>
   );
