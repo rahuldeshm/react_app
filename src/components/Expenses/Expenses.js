c717823ff990adb580acd99bd75fe7829cf2221b
rahul deshmukh <rahuldeshmukh4545@gmail.com>
2023-04-09 16:32:14 +0530

    1. Updated filter 2. removed bugs 3 added element for no item and one item in Expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,33 +6,46 @@ import ExpenseFilter from "./ExpenseFilter";
 function Expenses(props) {
   const arr = props.allExpenses;
   const [filteredYear, setFilteredYear] = useState("2021");
-  const [filteredArr, setfilterdArr] = useState(arr);
+
+  const filtrerd = arr.filter(
+    (el) => el.date.getFullYear().toString() === filteredYear
+  );
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    const filtrerd = arr.filter((el) => el.date.getFullYear() == selectedYear);
-    setfilterdArr(filtrerd);
   };
-  // console.log("in Expense", props.newExpense);
-
-  // allexpence.push(props.newExpense);
+  let allexpen = <p>No Expense Here</p>;
+  if (filtrerd.length === 1) {
+    allexpen = [
+      <ExpenseItem
+        key={filtrerd[0].id}
+        title={filtrerd[0].title}
+        amount={filtrerd[0].amount}
+        date={filtrerd[0].date}
+        locationofexpence={filtrerd[0].locationofexpence}
+      ></ExpenseItem>,
+      <p key={"one1"}>Only one expense add more</p>,
+    ];
+  } else if (filtrerd.length > 1) {
+    allexpen = filtrerd.map((e) => {
+      return (
+        <ExpenseItem
+          key={e.id}
+          title={e.title}
+          amount={e.amount}
+          date={e.date}
+          locationofexpence={e.locationofexpence}
+        ></ExpenseItem>
+      );
+    });
+  }
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       ></ExpenseFilter>
-      {filteredArr.map((e) => {
-        return (
-          <ExpenseItem
-            key={e.id}
-            title={e.title}
-            amount={e.amount}
-            date={e.date}
-            locationofexpence={e.locationofexpence}
-          ></ExpenseItem>
-        );
-      })}
+      {allexpen}
     </Card>
   );
 }
